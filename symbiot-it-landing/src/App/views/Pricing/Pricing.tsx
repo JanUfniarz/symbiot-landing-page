5b4a0a84852f6b58faad3c1cbef37a1e25a219c6
JanUfniarz <jan.ufniarz@gmail.com>
2025-07-07 21:35:30 +0200

    Pricing intro

--- symbiot-it-landing/src/App/views/Pricing/Pricing.tsx
+++ symbiot-it-landing/src/App/views/Pricing/Pricing.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 import AuditForm from "./components/AuditForm/AuditForm";
+import styles from './Pricing.module.css';
+import content from './Pricing_content.json'
 
 const Pricing = () => <>
     <br/>
+    <div className={styles.introContainer}>
+        <p className={styles.introText}>{content.intro}</p>
+    </div>
     <AuditForm/>
 </>
 
