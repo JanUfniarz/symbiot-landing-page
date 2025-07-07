5bcc65f73342cf0fc42f2617e73f2b2947fcb0c4
JanUfniarz <jan.ufniarz@gmail.com>
2025-07-07 23:10:57 +0200

    onhover card

--- symbiot-it-landing/src/App/views/Home/components/Solutions/Solutions.tsx
+++ symbiot-it-landing/src/App/views/Home/components/Solutions/Solutions.tsx
@@ -3,7 +3,7 @@ import content from './Solutions_content.json';
 import styles from './Solutions.module.css';
 
 const Solutions: React.FC = () => <section className={styles.solutionsSection}>
-    <h2 className={styles.title}>Nasze Rozwiązania</h2>
+    <h2 className={styles.title}>Nasza Oferta</h2>
     <div className={styles.solutionsContainer}> {
         content.map((solution, index) => (
             <div key={index} className={styles.solutionCard}>
